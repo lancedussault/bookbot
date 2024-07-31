804591d0ce1738ad563e8eae50b1dc5954c5e6ef
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-07-31 07:08:41 -0600

    Finished the porject by making a list of dictionaries, sorting them based off of count, and printed a report that included the file path, word count, and a list of each letter and the time it is used

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ def main():
     text = read_the_book(path_to_file)
     word_count = count_words(text)
     character_count = count_characters(text)
-    # print(text)
-    # print(word_count)
-    print(character_count)
+    dict_list = convert_dict_to_list(character_count)
+    dict_list.sort(reverse=True, key=sort_on)
+    #print(text)
+    #print(word_count)
+    #print(character_count)
+    #print(dict_list)
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    for item in dict_list:
+        print(f"The {item["char"]} character was found {item["count"]} times")
+    print("--- End report ---")
 
 def read_the_book(path):
     with open(path) as f:
@@ -19,13 +27,24 @@ def count_characters(text):
     char_int_dict = {}
     lowered_text = text.lower()
     for char in lowered_text:
-        char_int_dict[char] = 0
-        for key in char_int_dict:
-            if char_int_dict[key] == 0:
-                char_int_dict[key] = 1
-            else:
-                char_int_dict[key] += 1
-    # print(char_int_dict)
+        if char not in char_int_dict:
+            char_int_dict[char] = 1
+        else: 
+            char_int_dict[char] += 1
     return char_int_dict
+
+def convert_dict_to_list(dict):
+    dict_list = []
+    for key, value in dict.items():
+        if key.isalpha():
+            new_dict = {
+                'char': key,
+                'count': value
+            }
+            dict_list.append(new_dict)
+    return dict_list
+
+def sort_on(dict):
+    return dict["count"]
 
 main()
